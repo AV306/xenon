e8045b9c3884521a8ea1aeda04a5a8c08c89ab0b
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-01-17 03:00:59 +0000

    Command processor

--- src/main/java/me/av306/xenon/features/AutoReplyFeature.java
+++ src/main/java/me/av306/xenon/features/AutoReplyFeature.java
@@ -1,18 +1,27 @@
 package me.av306.xenon.features;
 
 import me.av306.xenon.Xenon;
-import me.av306.xenon.features.interfaces.IFeature;
+import me.av306.xenon.features.interfaces.*;
 
 
-public class AutoReplyFeature implements IFeature
+public class AutoReplyFeature implements IFeature, ICommand
 {
+		private static String message = ""; // FIXME: optimize this, stop using so much static shit
+	
     @Override
     public void onEnable()
     {
-        Xenon.INSTANCE.CLIENT.player.sendChatMessage( ":" );
+        Xenon.INSTANCE.CLIENT.player.sendChatMessage( message );
     }
 
 
     @Override
     public void onDisable() {}
+
+
+		@Override // FIXME
+		public static void processCommand( String[] command )
+		{
+			message = command[1];
+		}
 }
--- src/main/java/me/av306/xenon/mixins/ChatHudMixin.java
+++ src/main/java/me/av306/xenon/mixins/ChatHudMixin.java
@@ -1,15 +1,11 @@
 package me.av306.xenon.mixins;
 
-import me.av306.xenon.Xenon;
-import me.av306.xenon.features.CommandProcessorFeature;
 import net.minecraft.client.gui.hud.ChatHud;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 
 @Mixin( ChatHud.class )
 public class ChatHudMixin
@@ -21,18 +17,17 @@ public class ChatHudMixin
     )
     private void onAddMessage( Text chatText, int chatLineId, CallbackInfo ci )
     {
-        if ( CommandProcessorFeature.isEnabled && chatText.getString().startsWith("!") )
-        {
-            String command = chatText.getString().replace("!", " ").stripLeading();
-            if ( Xenon.INSTANCE.commandRegistry.registry.containsKey(command) )
-            {
-                Xenon.INSTANCE.commandRegistry.registry.get(command).execute(); // TODO
-                Xenon.INSTANCE.CLIENT.player.sendMessage(
-                        new LiteralText( String.format( "Command: %s", command ) ),
-                        false
-                );
-            }
-        }
+		 	String message = chatText.getString();
+			
+			// check if its a normal message
+			if ( message.startsWith("!") )
+			{
+				// seems to ba a command; try and execute
+				Xenon.INSTANCE.commandProcessor.execute( message );
+
+				// cancel message
+				ci.cancel();
+			}
     }
 
 }
