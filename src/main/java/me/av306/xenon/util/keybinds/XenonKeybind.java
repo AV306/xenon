9da2dba152255717f73027d76851813864e27eb5
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-01-21 01:56:38 +0000

    Fix

--- src/main/java/me/av306/xenon/util/keybinds/XenonKeybind.java
+++ src/main/java/me/av306/xenon/util/keybinds/XenonKeybind.java
@@ -5,7 +5,7 @@ import me.av306.xenon.features.interfaces.IFeature;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.util.InputUtil.Type;
 
 
 /**
@@ -18,7 +18,7 @@ public class XenonKeybind<T extends IFeature>
     public T feature;
 
 
-    public XenonKeybind( String translation, InputUtil type, int key, String category, T feature )
+    public XenonKeybind( String translation, InputUtil.Type type, int key, String category, T feature )
     {
       this.keybind = KeyBindingHelper.registerKeyBinding(
 				new KeyBinding( translation, type, key, category )
