10ec00d0f2397e7ddfe10b0758fbc1d8a284ea70
AV306 <brandonjoash@gmail.com>
2022-01-22 12:40:29 +0800

    panorama

---
+++ src/main/java/me/av306/xenon/features/TakePanoramaFeature.java
@@ -0,0 +1,21 @@
+package me.av306.xenon.features;
+
+import me.av306.xenon.Xenon;
+import me.av306.xenon.features.interfaces.IFeature;
+import net.minecraft.text.Text;
+
+import java.io.File;
+
+public class TakePanoramaFeature implements IFeature
+{
+    @Override
+    public void onEnable()
+    {
+        Text text = Xenon.INSTANCE.CLIENT.takePanorama( Xenon.INSTANCE.CLIENT.runDirectory, 1024, 1024 );
+
+        Xenon.INSTANCE.CLIENT.player.sendMessage( text, false );
+    }
+
+    @Override
+    public void onDisable() {}
+}
