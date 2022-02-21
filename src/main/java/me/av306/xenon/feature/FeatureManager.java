2adb30db3ccb1f109e5ae80193f0e901a92de790
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-02-21 10:20:49 +0800

    Update FeatureManager.java

--- src/main/java/me/av306/xenon/feature/FeatureManager.java
+++ src/main/java/me/av306/xenon/feature/FeatureManager.java
@@ -1,5 +1,19 @@
 package me.av306.xenon.feature;
 
+/**
+ * Xenon Feature Manager.
+ * Does stuff like have a BiMap for conversion of Feature name strings to classes.
+ * Also maintains an ArrayList of enabled features.
+ * Exposes most of these through instance methods and fields.
+ *
+ * Why am I wasting my life writing long docstrings?
+ * No one besides me is ever gonna look at this anyway.
+ * What's the point of all this?
+ * Why do I spend hours on end, trying to figure out
+ * how to implement and optimise stuff in this?
+ * 
+ * Anyways, have fun with this class, future self!
+ */  
 public class FeatureManager
 {
   
