b357493b3fc03a4943541a9ee483f8987a496e93
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-03-08 01:49:05 +0000

    Patch patch

--- src/main/java/me/av306/xenon/features/FeatureList.java
+++ src/main/java/me/av306/xenon/features/FeatureList.java
@@ -1,6 +1,6 @@
 package me.av306.xenon.features;
 
-import me.av306.xenon.featur.IToggleableFeature;
+import me.av306.xenon.feature.IToggleableFeature;
 import me.av306.xenon.Xenon;
 
 public class FeatureList extends IToggleableFeature
