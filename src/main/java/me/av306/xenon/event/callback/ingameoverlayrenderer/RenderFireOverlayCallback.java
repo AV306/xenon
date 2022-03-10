3433f4a6618ab4eb88ee7f64e1dff40821093fff
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-03-10 04:14:08 +0000

    petch

---
+++ src/main/java/me/av306/xenon/event/callback/ingameoverlayrenderer/RenderFireOverlayCallback.java
@@ -0,0 +1,40 @@
+package me.av306.xenon.event.callback.ingameoverlayrenderer;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.ActionResult;
+
+/**
+ * Callback for InGameOverlayRenderer.
+ * Triggered after the method(s) is/are invoked, before any function calls (i.e. HEAD).
+ *
+ * Upon return:
+ * - SUCCESS: Cancel further processing and render overlays as normal.
+ * - PASS: Continue to next listener and 
+ *	 default to SUCCESS when there are no other listeners.
+ * - FAIL: Cancel the event.
+ */
+public class RenderFireOverlayCallback
+{		
+	public static final Event<RenderFireOverlayCallback> EVENT = EventFactory.createArrayBacked(
+		RenderFireOverlayCallback.class,
+		(listeners) -> (client, matrices) ->
+		{
+			// iterate over each listener
+			for ( RenderFireOverlayCallback listener : listeners )
+			{
+				// let the listener interact with the event
+				ActionResult result = listener.interact( client, matrices );
+				// stop and return if the listener chooses not to pass it on
+				if ( result != ActionResult.PASS )
+					return result;
+			}
+
+			return ActionResult.PASS;
+		}
+	);
+
+	ActionResult interact( MinecraftClient client, MatrixStack matrices );
+}
