cd6f46e263294d5cf3172b7c32fa8301415c7a1e
Blender <66512356+AV306@users.noreply.github.com>
2022-03-23 04:31:41 +0000

    Broken

--- src/main/java/me/av306/xenon/util/text/TextUtil.java
+++ src/main/java/me/av306/xenon/util/text/TextUtil.java
@@ -4,20 +4,86 @@ import me.av306.xenon.util.ScreenPosition;
 import me.av306.xenon.Xenon;
 
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.util.Window;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 
 public class TextUtil
 {
-	// TODO: Maybe put text drawing logic here someday? im too lazy rn :P
+	// TODO: add more stuff
 	public static void drawPositionedText( MatrixStack matrices, Text text, ScreenPosition position, int color )
 	{
 		TextRenderer textRenderer = Xenon.INSTANCE.client.textRenderer;
+		int scaledWidth = Xenon.INSTANCE.client.window.getScaledWidth();
+		int scaledHeight = Xenon.INSTANCE.client.window.getScaledHeight();
 		switch ( position )
 		{
 			case TOP_LEFT:
+				textRenderer.drawWithShadow( matrices, text, 5, 5, color );
+				break;
+				
 			case TOP_CENTER:
+				// calculate x
+				int x = (scaledWidth - textRenderer.getWidth( text )) / 2;
+				textRenderer.drawWithShadow( matrices, text, 5, x, color );
+				break;
+				
 			case TOP_RIGHT:
+				// calculate x
+				int x = scaledWidth - textRenderer.getWidth( text );
+				break;
+				
+			case BOTTOM_LEFT:
+			case BOTTOM_CENTER:
+			case BOTTOM_RIGHT:
+				break;
+		}
+	}
+
+	public static void drawPositionedMultiLineText( MatrixStack matrices, Text[] text, ScreenPosition position, int yOffset, int color )
+	{
+		TextRenderer textRenderer = Xenon.INSTANCE.client.textRenderer;
+		int scaledWidth = Xenon.INSTANCE.client.window.getScaledWidth();
+		int scaledHeight = Xenon.INSTANCE.client.window.getScaledHeight();
+		int y = 0;
+		switch ( position )
+		{
+			case TOP_LEFT:
+				y = 5 + yOffset;
+        for ( Text text : texts )
+        {
+          textRenderer.drawWithShadow( matrices, text, 5, y, color );
+
+          y += 12;
+			  }
+				break;
+				
+			case TOP_CENTER:
+		    // calculate y
+		    y = 5 + yOffset;
+				
+		    for ( Text text : texts )
+        {
+					int x = (scaledWidth - textRenderer.getWidth( text )) / 2;
+					
+          textRenderer.drawWithShadow( matrices, text, x, y, color );
+
+          y += 12;
+			  }
+				break;
+				
+			case TOP_RIGHT:
+				// calculate x and y
+				for ( Text text : texts )
+        {
+					int x = scaledWidth - textRenderer.getWidth( text );
+					
+          textRenderer.drawWithShadow( matrices, text, x, y, color );
+
+          y += 12;
+			  }
+				break;
+				
 			case BOTTOM_LEFT:
 			case BOTTOM_CENTER:
 			case BOTTOM_RIGHT:
