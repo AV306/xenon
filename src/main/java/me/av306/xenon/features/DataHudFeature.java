d2499dbb6f62559734e9d61501f68481508c3f51
AV306 <brandonjoash@gmail.com>
2022-04-08 18:22:18 +0800

    Create DataHudFeature.java

---
+++ src/main/java/me/av306/xenon/features/DataHudFeature.java
@@ -0,0 +1,19 @@
+package me.av306.xenon.features;
+
+import me.av306.xenon.feature.IToggleableFeature;
+
+public class DataHudFeature extends IToggleableFeature
+{
+
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+}
