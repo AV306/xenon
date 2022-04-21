d0ba34b8b4846517366059ea6959ce8299e75c93
Blender <66512356+AV306@users.noreply.github.com>
2022-04-21 18:38:38 +0800

    bufix

--- src/main/java/me/av306/xenon/config/ConfigurationManager.java
+++ src/main/java/me/av306/xenon/config/ConfigurationManager.java
@@ -8,66 +8,64 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class ConfigurationManager
-{
+public class ConfigurationManager {
     // it's the feature's job to parse the setting string
     public HashMap<String, String> settings = new HashMap<>();
-    
+
     private final File configFile;
 
-    public ConfigurationManager( File configFile )
-    {
+    public ConfigurationManager(File configFile) {
+        Xenon.INSTANCE.LOGGER.info( "Now loading config file..." );
         this.configFile = configFile;
-			  
-			  try
-				{
-					this.configFile.createNewFile(); // ensure that the file exists
-				}
-			  catch ( IOException e )
-				{
-					Xenon.INSTANCE.LOGGER.warn( "Could not ensure the config file exists!" );
-					e.printStackTrace();
-				}
-			
-			  loadConfigs();
-		}
 
-    public void loadConfigs()
-    {
+        try
+        {
+            this.configFile.createNewFile(); // ensure that the file exists
+        }
+        catch ( IOException e )
+        {
+            Xenon.INSTANCE.LOGGER.warn("Could not ensure the config file exists!");
+            e.printStackTrace();
+        }
 
-			  // doesn't matter if this throws,
-				// it will just leave the HashMap empty,
-				// and the Features will just set their default configs.
-        try ( Scanner configFileScanner = new Scanner( this.configFile ); )
-				{
-          while ( configFileScanner.hasNextLine() )
-          {
-              String line = configFileScanner.next();
-              if ( line.startsWith( "#" ) ) continue; // skip comments
-              else
-              {
-                String[] config = line.strip().split( "=" );
-                // e.g. {"waila.updatefreq", "3"}
-                // then WAILA parses "3" to 3 (short)
-                settings.put( config[0], config[1] );
-              }
-          }
-			
-	  	    Xenon.INSTANCE.LOGGER.info( "Done reading configurations!" );
-				}
-			  catch ( FileNotFoundException e )
-				{
-					Xenon.INSTANCE.LOGGER.warn( "Config file absent! Features will use default settings." );
-				}
+        loadConfigs();
     }
 
-	  public void updateConfig( String key, String newValue )
-	  {
-      this.settings.put( key, newValue );
+    private void loadConfigs()
+    {
+        // doesn't matter if this throws,
+        // it will just leave the HashMap empty,
+        // and the Features will just set their default configs.
+        try ( Scanner configFileScanner = new Scanner( this.configFile ); )
+        {
+            while ( configFileScanner.hasNextLine() )
+            {
+                String line = configFileScanner.nextLine().strip();
+                if ( !line.startsWith( "#" ) && !line.isBlank() ) // skip comments
+                {
+                    String[] config = line.strip().split( "=" );
+                    // e.g. {"waila.updatefreq", "3"}
+                    // then WAILA parses "3" to 3 (short)
+                    Xenon.INSTANCE.LOGGER.info( "Read configuration: " + line );
+                    settings.put( config[0], config[1] );
+                }
+            }
 
-			// we'll only overwrite the affected line
-			// Iterate over the file till we find the correct line
-			//while ( this.configFileS)
-			// TODO: actually implement
-	  }
+            Xenon.INSTANCE.LOGGER.info( "Done reading configurations!" );
+        }
+        catch ( FileNotFoundException e )
+        {
+            Xenon.INSTANCE.LOGGER.warn( "Config file absent! Features will use default settings." );
+        }
+    }
+
+    public void updateConfig(String key, String newValue)
+    {
+        this.settings.put(key, newValue);
+
+        // we'll only overwrite the affected line
+        // Iterate over the file till we find the correct line
+        //while ( this.configFileS)
+        // TODO: actually implement
+    }
 }
