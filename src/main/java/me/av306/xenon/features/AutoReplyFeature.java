7ca22cad8392874d2a0ed0c5c4197fa6811b87c6
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-04-23 02:58:41 +0000

    Oops

--- src/main/java/me/av306/xenon/features/AutoReplyFeature.java
+++ src/main/java/me/av306/xenon/features/AutoReplyFeature.java
@@ -7,7 +7,7 @@ import me.av306.xenon.feature.IFeature;
 public class AutoReplyFeature extends IFeature
 {
 	private String message = "";
-	public static void setMessage( String newMessage) { message = newMessage; }
+	//public static void setMessage( String newMessage) { message = newMessage; }
 
 	public AutoReplyFeature()
 	{
