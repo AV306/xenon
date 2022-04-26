564b805e5a1a496a75ec729da4eacddc9ef05c6b
Blender <66512356+AV306@users.noreply.github.com>
2022-04-26 12:14:24 +0800

    fix

--- src/main/java/me/av306/xenon/config/feature/WailaGroup.java
+++ src/main/java/me/av306/xenon/config/feature/WailaGroup.java
@@ -8,5 +8,5 @@ public class WailaGroup implements ConfigGroup
 {
 	@ConfigEntry.BoundedInteger(min = 1, max = 1024)
 	@ConfigEntry.Slider
-	public static int interval = 3;
+	public static short interval = 3;
 }
