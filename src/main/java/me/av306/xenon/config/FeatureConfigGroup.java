a62bed61292d01d5efb0789a269d2ef145f00e23
Blender <66512356+AV306@users.noreply.github.com>
2022-04-26 10:38:49 +0800

    ;

--- src/main/java/me/av306/xenon/config/FeatureConfigGroup.java
+++ src/main/java/me/av306/xenon/config/FeatureConfigGroup.java
@@ -1,6 +1,6 @@
 package me.av306.xenon.config;
 
-import me.av306.xenon.config.feature.*
+import me.av306.xenon.config.feature.*;
 import me.lortseam.completeconfig.api.*;
 import me.lortseam.completeconfig.api.ConfigContainer.Transitive;
 
