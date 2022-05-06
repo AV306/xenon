77990abea95e642e66e459a6ba517a2e7579c48c
Blender <66512356+AV306@users.noreply.github.com>
2022-05-06 12:45:07 +0800

    sadf

--- src/main/java/me/av306/xenon/util/color/ColorUtil.java
+++ src/main/java/me/av306/xenon/util/color/ColorUtil.java
@@ -1,13 +1,16 @@
 package me.av306.xenon.util.color;
 
+/**
+ * Helper class for converting rgb arrays to int values, and vice versa.
+ */
 public class ColorUtil
 {
 	// AARRGGBB
-	public static final int WHITE = 0xFFFFFFFF;
-	public static final int BLACK = 0xFF000000;
-	public static final int RED = 0xFFFF0000;
-	public static final int GREEN = 0xFF00FF00;
-	public static final int BLUE = 0xFF0000FF;
+	public static final int WHITE = 0xFFFFFF;
+	public static final int BLACK = 0x000000;
+	public static final int RED = 0xFF0000;
+	public static final int GREEN = 0x00FF00;
+	public static final int BLUE = 0x0000FF;
 	
 	public static int rgbToInt( int r, int g, int b ) { return r << 16 | g << 8 | b; }
 
@@ -15,4 +18,23 @@ public class ColorUtil
     {
       return a << 24 | r << 16 | g << 8 | b;
     }
+
+	public static int[] intToRgb( int rgb )
+	{
+		return {
+				rgb >> 16 & 0xFF,
+				rgb >> 8 & 0xFF,
+				rgb & 0xFF
+		};
+	}
+
+	public static int[] intToArgb( int argb )
+	{
+		return {
+				argb >> 24 & 0xFF,
+				argb >> 16 & 0xFF,
+				argb >> 8 & 0xFF,
+				argb & 0xFF
+		};
+	}
 }
