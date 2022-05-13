ff445aad188dd25a4d0de0b26dfaede41954b85c
AV306 <brandonjoash@gmail.com>
2022-05-13 19:57:08 +0800

    4.0.0

--- src/main/java/me/av306/xenon/features/ProximityRadarFeature.java
+++ src/main/java/me/av306/xenon/features/ProximityRadarFeature.java
@@ -1,19 +1,20 @@
 package me.av306.xenon.features;
 
+import com.mojang.datafixers.kinds.IdF;
 import me.av306.xenon.Xenon;
 import me.av306.xenon.config.feature.ProximityRadarGroup;
 import me.av306.xenon.feature.IToggleableFeature;
-
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.util.ClientPlayerTickable;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
-import net.minecraft.text.LiteralText;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.text.*;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
+
+import java.text.Normalizer;
 
 
 public class ProximityRadarFeature extends IToggleableFeature
@@ -23,7 +24,6 @@ public class ProximityRadarFeature extends IToggleableFeature
     public ProximityRadarFeature()
     {
         super( "ProximityRadar" );
-        // tick callback in ClientPlayerTickable
         ClientTickEvents.END_WORLD_TICK.register( (object) -> this.tick() );
     }
 
@@ -57,19 +57,27 @@ public class ProximityRadarFeature extends IToggleableFeature
 
         if ( entity instanceof ProjectileEntity || entity instanceof HostileEntity )
         {
-            Xenon.INSTANCE.LOGGER.info( "Scanned entity at " + entity.getBlockPos() );
+            //Xenon.INSTANCE.LOGGER.info( "Scanned entity at " + entity.getBlockPos() );
 
             Vec3d entityPos = entity.getPos();
             Vec3d clientPos = Xenon.INSTANCE.client.player.getPos();
+            Double distance = entityPos.distanceTo( clientPos );
 
-            if (
-                    (entityPos.getX() < clientPos.getX() + range && entityPos.getX() > clientPos.getX() - range) &&
-                    (entityPos.getY() < clientPos.getY() + range && entityPos.getY() > clientPos.getY() - range) &&
-                    (entityPos.getZ() < clientPos.getZ() + range && entityPos.getZ() > clientPos.getZ() - range)
-            )
+            if ( distance < range )
             {
+                Text text = new TranslatableText(
+                        entity instanceof HostileEntity ?
+                                "text.xenon.proximityradar.hostile" :
+                                "text.xenon.proximityradar.projectile",
+                        distance.toString().substring( 0, 3 )
+                ).formatted( Formatting.RED, Formatting.BOLD );
                 // TODO: flash red vignette
-                Xenon.INSTANCE.client.player.sendMessage( new LiteralText("WARNING! PROJECTILE IN RANGE"),  true );
+                Xenon.INSTANCE.client.player.sendMessage( text,  true );
+                /*Xenon.INSTANCE.client.player.playSound(
+                        new SoundEvent( new Identifier( "block.note.ping" ) ),
+                        10f,
+                        1f
+                );*/
             }
         }
     }
