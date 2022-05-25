404179b90924e1efd9db043586781a822dad7210
Blender <66512356+AV306@users.noreply.github.com>
2022-05-25 12:54:23 +0800

    in-progress backup

---
+++ src/main/java/me/av306/xenon/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,26 @@
+package me.av306.xenon.mixin;
+
+import com.mojang.authlib.GameProfile;
+import me.av306.xenon.config.feature.HighJumpGroup;
+import me.av306.xenon.features.HighJumpFeature;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ClientPlayerEntity.class )
+public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity
+{
+    public ClientPlayerEntityMixin( ClientWorld world, GameProfile profile )
+    {
+        super(world, profile);
+    }
+
+    @Override
+    public float getJumpVelocity()
+    {
+        return super.getJumpVelocity();
+    }
+
+
+}
