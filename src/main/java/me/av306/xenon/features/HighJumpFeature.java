6aeed9c0b116447a8e3f90c281b6f32ba3841e92
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-05-27 10:41:05 +0800

    hope this works

--- src/main/java/me/av306/xenon/features/HighJumpFeature.java
+++ src/main/java/me/av306/xenon/features/HighJumpFeature.java
@@ -32,5 +32,6 @@ public class HighJumpFeature extends IToggleableFeature
     @Override
     protected void onDisable()
     {
+        EventFields.JUMP_VELOCITY_MODIFIER -= HighJumpGroup.height;
     }
 }
