bdba868e3f42cf9febf69c1417a4e67c6068bc2c
Blender <66512356+AV306@users.noreply.github.com>
2022-06-11 20:06:56 +0800

    Update SimpleOptionMixin.java

--- src/main/java/me/av306/xenon/mixin/SimpleOptionMixin.java
+++ src/main/java/me/av306/xenon/mixin/SimpleOptionMixin.java
@@ -21,9 +21,19 @@ public abstract class SimpleOptionMixin<T> implements SimpleOptionAccessor<T>
     @Final
     private Consumer<T> changeCallback;
 
+
+    // Ugh.
     @Override
     public void forceSetValue(T newValue)
     {
+        // This is basically what akready exists in setValue,
+        // minus the validation.
+        // Why, Mojang? Why?
+        // First the text system, and now this!
+        // Why must you do this to us?
+        // I mean, I get why you would do this (and the text system too),
+        // but it makes life really difficult.
+        // Well, what's done is done, and this is how we have to do it now.
         if( !Xenon.INSTANCE.client.isRunning() )
         {
             value = newValue;
