ee5cb14215c5ce11dce8c50ba9fcb360ac397025
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-07-06 14:51:45 +0800

    fix update checker

--- src/main/java/me/av306/xenon/util/update/UpdateChecker.java
+++ src/main/java/me/av306/xenon/util/update/UpdateChecker.java
@@ -2,10 +2,49 @@ package me.av306.xenon.util.update;
 
 import me.av306.xenon.Xenon;
 
+import java.net.*;
+
 public class UpdateChecker
 {
-    public static boolean checkForUpdate( String url )
+    public static boolean checkForUpdate( String urlString )
     {
+        return getLatestVersion( urlString ) > getCurrentVersion();
+    }
+
+    private int getLatestVersion( String urlString )
+    {
+        int latestVer = 0;
+        try
+        {
+            URL api = new URL( urlString );
+
+            HttpUrlConnection connection = (HttpUrlConnection) api.openConnection(); // IOException
+            connection.setRequestMethod( "GET" );
+            connection.connect(); // SocketTimeoutException or IOException
+
+            // response code!
+            int responseCode = connection.getResponseCode();
+
+            if ( responseCode != 200 )
+                Xenon.INSTANCE.LOGGER.warn( "Oh no! Update check returned code {} and this is very bad!!!", responseCode );
+            else
+            {
+                // we are connecting to my intermediary server,
+                // which will perform the backend bookkeeping on the tags.
+                // It will return the latest version as a integer.
+                // E.g. "4.3.1+1.19.x" -> 431
+                // "4.4.0+1.19.x" -> 440
+                // These can then be compared to the current version easily.
+                latestVer = (int) connection.getContent(); // CCE
+            }
+        }
+        catch ( Exception e )
+        {
+            Xenon.INSTANCE.LOGGER.warn( e );
+        }
+
+        return latestVer;
+
         /*
         String latestTag;
         try
@@ -39,6 +78,23 @@ public class UpdateChecker
         }
 
         return latestVersion > currentVersion;*/
-        return false;
+    }
+
+    private int getCurrentVersion()
+    {
+        int temp = 0;
+
+        try
+        {
+            // "4.3.1" -> "431" -> 431
+            temp = Integer.parseInt( Xenon.INSTANCE.version.replaceAll( "." ).trim() );
+        }
+        catch ( NumberFormatException nfe )
+        {
+            Xenon.INSTANCE.warn( nfe );
+            temp = 0;
+        }
+
+        return temp;
     }
 }
