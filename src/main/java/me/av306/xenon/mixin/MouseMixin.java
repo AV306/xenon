cf9d5e92050d44d121eb9af5bf7bb7ca4f1ecc0b
Blender <66512356+AV306@users.noreply.github.com>
2022-08-10 19:50:25 +0800

    studd

---
+++ src/main/java/me/av306/xenon/mixin/MouseMixin.java
@@ -0,0 +1,25 @@
+package me.av306.xenon.mixin;
+
+import me.av306.xenon.event.MouseScrollEvent;
+import net.minecraft.client.Mouse;
+import net.minecraft.util.ActionResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin( Mouse.class )
+public class MouseMixin
+{
+    @Inject(
+            at = @At("RETURN"),
+            method = "onMouseScroll(JDD)V",
+            cancellable = true
+    )
+    private void onOnMouseScroll( long window, double horizontal, double vertical, CallbackInfo ci )
+    {
+        ActionResult result = MouseScrollEvent.EVENT.invoker().interact( window, horizontal, vertical );
+
+        if ( result == ActionResult.FAIL ) ci.cancel();
+    }
+}
