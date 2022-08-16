2b4b9c9c12bafa956fe7b94cead668f70552054e
Blender <66512356+AV306@users.noreply.github.com>
2022-08-16 18:56:46 +0800

    ASDF

--- src/main/java/me/av306/xenon/config/feature/ZoomGroup.java
+++ src/main/java/me/av306/xenon/config/feature/ZoomGroup.java
@@ -1,17 +1,16 @@
 package me.av306.xenon.config.feature;
 
 import me.av306.xenon.config.XenonConfigGroup;
-import me.av306.xenon.util.ScreenPosition;
 import me.lortseam.completeconfig.api.ConfigEntries;
 import me.lortseam.completeconfig.api.ConfigEntry;
 
 @ConfigEntries( includeAll = true )
 public class ZoomGroup implements XenonConfigGroup
 {
-	@ConfigEntry.BoundedDouble( min = 2d, max = 100d )
-	@ConfigEntry.Slider
-	public static Double minZoom = 2d;
+	@ConfigEntry.BoundedDouble( min = 1.5d, max = 100d )
+	//@ConfigEntry.Slider
+	public static double minZoom = 2d;
 
-    @ConfigEntry.BoundedDouble( min = 2d, max = 100d )
-    public static Double maxZoom = 60d;
+    @ConfigEntry.BoundedDouble( min = 1.5d, max = 100d )
+    public static double maxZoom = 60d;
 }
