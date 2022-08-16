fdbc4d32ee527e0ba3675f67da9083a4493463a1
AV306 <brandonjoash@gmail.com>
2022-08-16 13:56:36 +0800

    qw

--- src/main/java/me/av306/xenon/config/feature/MultiQuickChatGroup.java
+++ src/main/java/me/av306/xenon/config/feature/MultiQuickChatGroup.java
@@ -2,8 +2,10 @@ package me.av306.xenon.config.feature;
 
 import me.av306.xenon.config.XenonConfigGroup;
 import me.lortseam.completeconfig.api.ConfigEntries;
-import me.lortseam.completeconfig.api.ConfigEntry;
-import net.minecraft.client.util.InputUtil.Key;
+import net.minecraft.client.util.InputUtil;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ConfigEntries( includeAll = true )
 public class MultiQuickChatGroup implements XenonConfigGroup
