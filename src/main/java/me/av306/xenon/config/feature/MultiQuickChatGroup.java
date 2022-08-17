442726ba4b698e7514dd219239176866c937327f
AV306 <brandonjoash@gmail.com>
2022-08-17 19:16:58 +0800

    yoooooooo

--- src/main/java/me/av306/xenon/config/feature/MultiQuickChatGroup.java
+++ src/main/java/me/av306/xenon/config/feature/MultiQuickChatGroup.java
@@ -2,13 +2,14 @@ package me.av306.xenon.config.feature;
 
 import me.av306.xenon.config.XenonConfigGroup;
 import me.lortseam.completeconfig.api.ConfigEntries;
+import net.minecraft.client.util.InputUtil;
 
 import java.util.HashMap;
-import java.util.Map;
 
 @ConfigEntries( includeAll = true )
 public class MultiQuickChatGroup implements XenonConfigGroup
 {
     // a GLFW int keycode is mapped to each message
-    public static Map<Integer, String> messages = new HashMap<>();
+    @ConfigEntries.Exclude
+    public static HashMap<InputUtil.Key, String> messages = new HashMap<>();
 }
