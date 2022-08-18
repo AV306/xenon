3b83d2701376bad7aa50a6c9078182251895f8da
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-08-18 12:14:50 +0800

    optimise the way feature names are updated, work on mqc

--- src/main/java/me/av306/xenon/config/feature/MultiQuickChatGroup.java
+++ src/main/java/me/av306/xenon/config/feature/MultiQuickChatGroup.java
@@ -4,12 +4,30 @@ import me.av306.xenon.config.XenonConfigGroup;
 import me.lortseam.completeconfig.api.ConfigEntries;
 import net.minecraft.client.util.InputUtil;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 @ConfigEntries( includeAll = true )
 public class MultiQuickChatGroup implements XenonConfigGroup
 {
     // a GLFW int keycode is mapped to each message
+
+    // Map method
     @ConfigEntries.Exclude
-    public static HashMap<InputUtil.Key, String> messages = new HashMap<>();
+    public static HashMap<InputUtil.Key, String> messageMap = new HashMap<>();
+
+    // Array method
+    public static ArrayList<String> messageArray = new ArrayList<>();
+
+    // hardcode message
+    public static String message0;
+    public static String message1;
+    public static String message2;
+    public static String message3;
+    public static String message4;
+    public static String message5;
+    public static String message6;
+    public static String message7;
+    public static String message8;
+    public static String message9;
 }
--- src/main/java/me/av306/xenon/features/chat/MultiQuickChatFeature.java
+++ src/main/java/me/av306/xenon/features/chat/MultiQuickChatFeature.java
@@ -16,7 +16,7 @@ public class MultiQuickChatFeature extends IFeature
 
         KeyEvent.EVENT.register( this::onKey );
 
-        MultiQuickChatGroup.messages.put( InputUtil.fromKeyCode( InputUtil.GLFW_KEY_1, -1 ), "asdfsdaf" );
+        //MultiQuickChatGroup.messagesMap.put( InputUtil.fromKeyCode( InputUtil.GLFW_KEY_1, -1 ), "asdfsdaf" );
     }
 
     @Override
@@ -24,7 +24,7 @@ public class MultiQuickChatFeature extends IFeature
 
     protected ActionResult onKey(
         long window,
-        int key,
+        int keycode,
         int scancode,
         int action,
         int modifiers
@@ -33,15 +33,51 @@ public class MultiQuickChatFeature extends IFeature
 		if ( this.keyBinding.isPressed() && action == GLFW.GLFW_PRESS )
         {
             // key was pressed while holding our key
+
             // TODO: Find a way to add optional configs in Cloth Config
             // Well, CC has support for Map...
+            // Ok, it doesn't work.
 
-            String message = MultiQuickChatGroup.messages.get( InputUtil.fromKeyCode( key, scancode ) );
+            // Map method
+            /*String message = MultiQuickChatGroup.messageMap.get( InputUtil.fromKeyCode( key, scancode ) );
 
             try
             {
                 assert Xenon.INSTANCE.client.player != null;
                 Xenon.INSTANCE.client.player.sendChatMessage( message );
+            }
+            catch ( NullPointerException ignored ) {}*/
+
+            // Array method
+            /*int key = keycode - 48;
+            String message = MultiQuickChatGroup.messageArray.get( key );*/
+
+            // Hardcode method
+            String message;
+            switch ( key )
+            {
+                case GLFW.GLFW_KEY_0 -> { message = MultiQuickChatGroup.message0; }
+                case GLFW.GLFW_KEY_1 -> { message = MultiQuickChatGroup.message1; }
+                case GLFW.GLFW_KEY_2 -> { message = MultiQuickChatGroup.message2; }
+                case GLFW.GLFW_KEY_3 -> { message = MultiQuickChatGroup.message3; }
+                case GLFW.GLFW_KEY_4 -> { message = MultiQuickChatGroup.message4; }
+                case GLFW.GLFW_KEY_5 -> { message = MultiQuickChatGroup.message5; }
+                case GLFW.GLFW_KEY_6 -> { message = MultiQuickChatGroup.message6; }
+                case GLFW.GLFW_KEY_7 -> { message = MultiQuickChatGroup.message7; }
+                case GLFW.GLFW_KEY_8 -> { message = MultiQuickChatGroup.message8; }
+                case GLFW.GLFW_KEY_9 -> { message = MultiQuickChatGroup.message9; }
+                default -> {}
+            }
+
+            // send the message
+            try
+            {
+                // throws NPE if message is null
+                Xenon.INSTANCE.client.player.sendChatMessage( message );
+
+                // Cancel keypress if message was present and sent successfully
+                // Will not be reached if message was not sent
+                return ActionResult.FAIL;
             }
             catch ( NullPointerException ignored ) {}
         }
