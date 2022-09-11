36d900a1c5abfb327dfcef464b2557d49835f035
AV306 <brandonjoash@gmail.com>
2022-09-11 21:15:09 +0800

    feature: red reticle alpha

--- src/main/java/me/av306/xenon/features/render/AimAssistFeature.java
+++ src/main/java/me/av306/xenon/features/render/AimAssistFeature.java
@@ -1,10 +1,15 @@
 package me.av306.xenon.features.render;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import me.av306.xenon.Xenon;
 import me.av306.xenon.event.RenderCrosshairEvent;
 import me.av306.xenon.feature.IToggleableFeature;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
 
 public class AimAssistFeature extends IToggleableFeature
 {
@@ -16,7 +21,28 @@ public class AimAssistFeature extends IToggleableFeature
 
     private ActionResult onStartRenderCrosshair( MatrixStack matrixStack )
     {
-        RenderSystem.setShaderColor( 1f, 0f, 0f, 1f );
+        RenderSystem.disableBlend();
+        HitResult hitResult = Xenon.INSTANCE.client.crosshairTarget;
+        if ( hitResult.getType() == HitResult.Type.ENTITY )
+        {
+            Entity entity = ((EntityHitResult) hitResult).getEntity();
+
+            if ( entity.isTeammate( Xenon.INSTANCE.client.player ) )
+            {
+                // Friendly!
+                // Green crosshair
+                RenderSystem.setShaderColor( 0f, 1f, 0f, 1f );
+            }
+            else if ( entity instanceof HostileEntity )
+            {
+                // Hostile and enemy!
+                // Red crosshair
+                RenderSystem.setShaderColor( 1f, 0f, 0f, 1f );
+            }
+        }
+
+        RenderSystem.enableBlend();
+
         return ActionResult.PASS;
     }
 
