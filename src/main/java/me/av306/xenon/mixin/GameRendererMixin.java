68857c29a580248ed622195b34b28b89e9a18158
AV306 <brandonjoash@gmail.com>
2022-11-18 13:09:11 +0800

    esp for proxradar works

--- src/main/java/me/av306/xenon/mixin/GameRendererMixin.java
+++ src/main/java/me/av306/xenon/mixin/GameRendererMixin.java
@@ -1,22 +1,27 @@
 package me.av306.xenon.mixin;
 
 import me.av306.xenon.event.EventFields;
+import me.av306.xenon.event.GameRenderEvents;
 import me.av306.xenon.event.GetFovEvent;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.resource.SynchronousResourceReloader;
+import net.minecraft.util.ActionResult;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin( GameRenderer.class )
-public class GameRendererMixin implements AutoCloseable, SynchronousResourceReloader
+public abstract class GameRendererMixin implements AutoCloseable, SynchronousResourceReloader
 {
-    @Shadow @Final private Camera camera;
+    @Shadow public abstract void reset();
 
     @Inject(
             at = @At( value = "RETURN", ordinal = 1 ),
@@ -30,6 +35,24 @@ public class GameRendererMixin implements AutoCloseable, SynchronousResourceRelo
         else cir.setReturnValue( (cir.getReturnValue() + EventFields.FOV_MODIFIER) / EventFields.FOV_ZOOM_LEVEL );
 
         //Xenon.INSTANCE.sendInfoMessage( String.valueOf( cir.getReturnValue() ) );
+    }
+
+   @Inject(
+           at = @At(
+                   value = "FIELD",
+                   target = "Lnet/minecraft/client/render/GameRenderer;renderHand:Z",
+                   opcode = Opcodes.GETFIELD,
+                   ordinal = 0
+           ),
+           method = "renderWorld(FJLnet/minecraft/client/util/math/MatrixStack;)V",
+           cancellable = true
+   )
+    private void onRenderWorld( float tickDelta, long limitTime, MatrixStack matrices, CallbackInfo ci )
+    {
+        ActionResult result = GameRenderEvents.RENDER_WORLD.invoker()
+                .onRenderWorld( tickDelta, limitTime, matrices );
+
+        if ( result == ActionResult.FAIL ) ci.cancel();
     }
 
     @Shadow
