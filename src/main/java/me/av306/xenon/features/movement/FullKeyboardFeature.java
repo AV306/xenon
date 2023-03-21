bdb3c733c1181315951b0bd530e45cfd466026f5
AV3_08 <brandonjoash@gmail.com>
2023-03-21 03:17:41 +0000

    yay

--- src/main/java/me/av306/xenon/features/movement/FullKeyboardFeature.java
+++ src/main/java/me/av306/xenon/features/movement/FullKeyboardFeature.java
@@ -1,7 +1,10 @@
 package me.av306.xenon.features.movement;
 
+import me.av306.xenon.Xenon;
 import me.av306.xenon.event.KeyEvent;
 import me.av306.xenon.feature.IToggleableFeature;
+import me.av306.xenon.mixin.MouseAccessor;
+import org.lwjgl.glfw.GLFW;
 
 import net.minecraft.util.ActionResult;
 
@@ -22,13 +25,18 @@ public final class FullKeyboardFeature extends IToggleableFeature
         int modifiers
     )
     {
-    //if ( !this.isEnabled ) return ActionResult.PASS;
-
+        if ( !this.isEnabled ) return ActionResult.PASS;
+        double f = Xenon.INSTANCE.client.options.getMouseSensitivity().getValue() * 32d;
         switch ( keycode )
         {
-            case GLFW.GLFW_KEY_UP -> ((MouseAccessor) Xenon.INSTANCE.client.mouse).changeY( 5 );
-            case GLFW.GLFW_KEY_DOWN -> ((MouseAccessor) Xenon.INSTANCE.client.mouse).changeY( -5 );
+            case GLFW.GLFW_KEY_UP -> ((MouseAccessor) Xenon.INSTANCE.client.mouse).setCursorDeltaY( -f );
+            case GLFW.GLFW_KEY_DOWN -> ((MouseAccessor) Xenon.INSTANCE.client.mouse).setCursorDeltaY( f );
+            case GLFW.GLFW_KEY_LEFT -> ((MouseAccessor) Xenon.INSTANCE.client.mouse).setCursorDeltaX( -f );
+            case GLFW.GLFW_KEY_RIGHT -> ((MouseAccessor) Xenon.INSTANCE.client.mouse).setCursorDeltaX( f );
+            default -> {}
         }
+
+        return ActionResult.PASS;
     }
 
     @Override
