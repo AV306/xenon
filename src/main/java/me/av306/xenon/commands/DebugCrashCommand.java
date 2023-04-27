6b92af01ca1a649ddc2a66e53fd2f819b5e37891
AV3_08 <66512356+AV306@users.noreply.github.com>
2023-04-27 13:01:16 +0800

    save

--- src/main/java/me/av306/xenon/commands/DebugCrashCommand.java
+++ src/main/java/me/av306/xenon/commands/DebugCrashCommand.java
@@ -12,8 +12,12 @@ public class DebugCrashCommand extends Command
 	@Override
 	public void execute( String[] args )
 	{
-		if ( args[0].equals( "confirm" ) )
-			GlfwUtil.makeJvmCrash();
-		else Xenon.INSTANCE.sendWarningMessage( "text.xenon.command.debugcrash.warning" );
+		Xenon.INSTANCE.LOGGER.warn( "The following out-of-bounds access is intentional." );
+		// Do an out-of-bounds access to crash
+		args[args.length] = "";
+
+		//if ( args[0].equals( "confirm" ) )
+		//	GlfwUtil.makeJvmCrash(); // DOn't use this because it won't save the world
+		//else Xenon.INSTANCE.sendWarningMessage( "text.xenon.command.debugcrash.warning" );
 	}
 }
