0a4149dc21e468f3a4f36cfdbef814341bd3bf7d
AV3_08 <brandonjoash@gmail.com>
2023-07-04 05:22:47 +0000

    We might need to downgrade CC

--- src/main/java/me/av306/xenon/config/feature/BlackBoxGroup.java
+++ src/main/java/me/av306/xenon/config/feature/BlackBoxGroup.java
@@ -17,4 +17,16 @@ public class BlackBoxGroup implements XenonConfigGroup
 	@ConfigEntry.BoundedInteger( min = 1, max = 100 )
 	@ConfigEntry.Slider
 	public static int trackingLogInterval = 5;
+
+	@ConfigEntry.BoundedInteger( min = 1, max = 100 )
+	@ConfigEntry.Slider
+	public static int writeInterval = 5;
+
+	@ConfigEntry.Boolean
+	//@ConfigEntry.Checkbox
+	public static boolean writeEntireQueue = false;
+
+	@ConfigEntry.BoundedInteger( min = 1, max = 100 )
+	@ConfigEntry.Slider
+	public static int bufferSize = 5;
 }
