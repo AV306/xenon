7de437c714900b142ae9e8699d14d5c32195d265
Blender <66512356+AV306@users.noreply.github.com>
2023-07-18 03:29:54 +0000

    just a little bit to relieve boredom

--- src/main/java/me/av306/xenon/event/GetJumpVelocityEvent.java
+++ src/main/java/me/av306/xenon/event/GetJumpVelocityEvent.java
@@ -2,7 +2,6 @@ package me.av306.xenon.event;
 
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
-import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 
 public interface GetJumpVelocityEvent
