814fd6c25c3270ec9c28e3e1bc1b71a34198f573
Blender <66512356+AV306@users.noreply.github.com>
2023-08-03 06:02:34 +0000

    alias command

---
+++ src/main/java/me/av306/xenon/commands/AliasCommand.java
@@ -0,0 +1,42 @@
+package me.av306.xenon.commands;
+
+import me.av306.xenon.Xenon;
+import me.av306.xenon.command.Command;
+import me.av306.xenon.feature.IFeature;
+import me.av306.xenon.util.KeybindUtil;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.input.Input;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+
+public class AliasCommand extends Command
+{
+    public AliasCommand()
+    {
+        super( "alias" );
+    }
+
+    @Override
+	public void execute( String[] args )
+	{
+		// args[0] is feature name, args[1] is new alias
+        try
+        {
+            IFeature feature = Xenon.INSTANCE.featureRegistry.get( args[0] );
+            String alias = args[1];
+
+            Xenon.INSTANCE.featureRegistry.put( alias, feature );
+
+            Xenon.INSTANCE.sendInfoMessage( "text.xenon.command.alias.success", args[0] );
+        }
+        catch ( NullPointerException npe )
+        {
+            // FIXME: We have a lot of repeated messages
+            Xenon.INSTANCE.sendErrorMessage( "text.xenon.command.unresolvable", this.name, args[0] );
+        }
+        catch ( ArrayIndexOutOfBoundsException oobe )
+        {
+            Xenon.INSTANCE.sendErrorMessage( "text.xenon.command.notenoughargs", this.name, args.length, 2 );
+        }
+	}
+}
--- src/main/java/me/av306/xenon/commands/BindCommand.java
+++ src/main/java/me/av306/xenon/commands/BindCommand.java
@@ -3,7 +3,10 @@ package me.av306.xenon.commands;
 import me.av306.xenon.Xenon;
 import me.av306.xenon.command.Command;
 import me.av306.xenon.feature.IFeature;
+import me.av306.xenon.util.KeybindUtil;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.input.Input;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 
 public class BindCommand extends Command
@@ -22,8 +25,14 @@ public class BindCommand extends Command
             IFeature feature = Xenon.INSTANCE.featureRegistry.get( args[0] );
             char key = args[1].charAt( 0 );
 
-            // Idk what the scancodes are :(
-            feature.getKeyBinding().setBoundKey( InputUtil.fromKeyCode( key, -1 ) );
+            feature.setKeyBinding( KeybindUtil.registerKeybind(
+				            "key.xenon." + feature.getName()
+                                    .toLowerCase()
+                                    .replaceAll( " ", "" ),
+				            key,
+				            "category.xenon." + feature.getCategory()
+		            )
+            );
 
             Xenon.INSTANCE.sendInfoMessage( "text.xenon.command.bind.success", args[0], key );
         }
