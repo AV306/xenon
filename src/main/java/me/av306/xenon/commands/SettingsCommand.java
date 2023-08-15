ab0c8f5f2cdf857b753907dc51f3b72f375fe57d
AV3_08 <brandonjoash@gmail.com>
2023-08-15 01:05:22 +0000

    i forgot what i was doing

---
+++ src/main/java/me/av306/xenon/commands/SettingsCommand.java
@@ -0,0 +1,25 @@
+package me.av306.xenon.commands;
+
+import me.av306.xenon.Xenon;
+import me.av306.xenon.command.Command;
+import net.minecraft.client.gui.screen.option.OptionsScreen;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Util;
+
+public class SettingsCommand extends Command
+{
+    public SettingsCommand()
+    {
+        super( "settings" );
+    }
+    
+    @Override
+    public void execute( String[] args )
+    {
+        /*Xenon.INSTANCE.client.setScreen(
+            new OptionsScreen( Xenon.INSTANCE.client.currentScreen, Xenon.INSTANCE.client.options )
+        );*/
+        //Util.throwOrPause( new RuntimeException() );
+        Xenon.INSTANCE.client.openPauseMenu( true );
+    }
+}
