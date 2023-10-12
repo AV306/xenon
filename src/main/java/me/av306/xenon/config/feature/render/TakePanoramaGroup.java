7f76b48450f5308803bb76aa3afb773fe0091e00
AV3_08 <66512356+AV306@users.noreply.github.com>
2023-10-12 10:36:08 +0800

    translations

--- src/main/java/me/av306/xenon/config/feature/render/TakePanoramaGroup.java
+++ src/main/java/me/av306/xenon/config/feature/render/TakePanoramaGroup.java
@@ -6,7 +6,7 @@ import me.lortseam.completeconfig.api.*;
 @ConfigEntries( includeAll = true )
 public class TakePanoramaGroup implements XenonConfigGroup
 {
-	@ConfigEntry.BoundedInteger(min = 4, max = 4096)
+	@ConfigEntry.BoundedInteger( min = 4, max = 8192 )
 	@ConfigEntry.Slider
-	public static int resolution = 1024;
+	public static int resolution = 4096;
 }
