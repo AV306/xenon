a0db767d81bc66d42e8de3cd6b3e5d838785f2c3
Blender <66512356+AV306@users.noreply.github.com>
2023-11-28 15:37:12 +0100

    Move FOV-related EventFields stuff into GetFovEvent
    Try to make EventFields stuff more self-documenting by moving them into the event classes themselves

--- src/main/java/me/av306/xenon/event/GetFovEvent.java
+++ src/main/java/me/av306/xenon/event/GetFovEvent.java
@@ -5,9 +5,9 @@ import net.fabricmc.fabric.api.event.EventFactory;
 import net.minecraft.client.render.Camera;
 import net.minecraft.util.ActionResult;
 
-public interface GetFovEvent
+public abstract class GetFovEvent
 {
-    Event<GetFovEvent> EVENT = EventFactory.createArrayBacked(
+    public static final Event<GetFovEvent> EVENT = EventFactory.createArrayBacked(
             GetFovEvent.class,
             (listeners) -> (camera, tickDelta, changingFov) ->
             {
@@ -21,6 +21,11 @@ public interface GetFovEvent
                 return ActionResult.PASS;
             }
     );
+
+    public static boolean SHOULD_OVERRIDE_FOV = false;
+    public static double FOV_OVERRIDE = 90d;
+    public static double FOV_MODIFIER = 0d;
+    public static double FOV_MULTIPLIER = 1d;
 
     ActionResult interact( Camera camera, float tickDelta, boolean changingFov );
 }
