980939d87cd2f79d079fb60e54d6b281dca707b1
AV306 <brandonjoash@gmail.com>
2024-02-15 18:55:13 +0800

    e

---
+++ src/main/java/me/av306/xenon/MainInitializer.java
@@ -0,0 +1,15 @@
+package me.av306.xenon;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MainInitializer implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+		Xenon.INSTANCE.LOGGER.info( "Doing server-side initialisation..." );
+
+		// TODO
+		// Registerr
+	}
+}
