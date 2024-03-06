b9b1f813f641728dcceaf8562a63f08d53121610
Blender <66512356+AV306@users.noreply.github.com>
2024-03-06 01:58:33 +0000

    I DID IT WOOO

--- src/main/java/me/av306/xenon/commands/DebugCrashCommand.java
+++ src/main/java/me/av306/xenon/commands/DebugCrashCommand.java
@@ -2,6 +2,7 @@ package me.av306.xenon.commands;
 
 import me.av306.xenon.Xenon;
 import me.av306.xenon.command.Command;
+import net.minecraft.client.Keyboard;
 
 public class DebugCrashCommand extends Command
 {
@@ -10,12 +11,14 @@ public class DebugCrashCommand extends Command
 	@Override
 	public void execute( String[] args )
 	{
-		Xenon.INSTANCE.LOGGER.warn( "The following out-of-bounds access is intentional." );
-		// Do an out-of-bounds access to crash
+		//Xenon.INSTANCE.LOGGER.warn( "The following out-of-bounds access is intentional." );
+
+		// MC catches the OOBE :(
 		args[args.length] = "";
+		//Xenon.INSTANCE.client.keyboard.pollDebugCrash();
 
 		//if ( args[0].equals( "confirm" ) )
-		//	GlfwUtil.makeJvmCrash(); // DOn't use this because it won't save the world
+		//	GlfwUtil.makeJvmCrash(); // Don't use this because it won't save the world
 		//else Xenon.INSTANCE.sendWarningMessage( "text.xenon.command.debugcrash.warning" );
 	}
 }
